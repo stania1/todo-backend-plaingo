4dcbd516587d28a21f15af30558547beee6a8e07
Sofia Tania <stania.ang@gmail.com>
2014-08-22 22:42:00 +0800

    Defined a new type, MyServer

--- src/todo.go
+++ src/todo.go
@@ -5,11 +5,38 @@ import (
   "net/http"
 )
 
-func handler(writer http.ResponseWriter, reader *http.Request) {
-  fmt.Fprintf(writer, "Hello world! %s", reader.URL.Path[1:])
+type MyServer struct {
+}
+
+func (server *MyServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+  fmt.Println(r.Method)
+  if origin := r.Header.Get("Origin"); origin != "" {
+    w.Header().Set("Access-Control-Allow-Origin", origin)
+  }
+
+  if r.Method == "OPTIONS" {
+    return
+  }
+
+  w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, GET, DELETE")
+  w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type")
+  fmt.Fprintf(w, "Hello world! %s", r.URL.Path[1:])
+}
+
+func corsHandler(h http.Handler) http.HandlerFunc {
+  return func(w http.ResponseWriter, r *http.Request) {
+    if (r.Method == "OPTIONS") {
+      //handle preflight in here
+    } else {
+      h.ServeHTTP(w,r)
+    }
+  }
 }
 
 func main() {
-  http.HandleFunc("/", handler)
+  server := new(MyServer)
+  fmt.Println("test")
+  http.HandleFunc("/", corsHandler(server))
   http.ListenAndServe(":8080", nil)
 }
