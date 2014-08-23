ef7417528a8186004ee0095a98afbf87b2ae71de
Sofia Tania <stania.ang@gmail.com>
2014-08-23 21:57:29 +0800

    Stored todos in memory

--- src/todo/todo_server.go
+++ src/todo/todo_server.go
@@ -3,14 +3,20 @@ package main
 import (
   "fmt"
   "net/http"
-  // "encoding/json"
+  "encoding/json"
   "io/ioutil"
 )
 
 type TodoItem struct {
-  Title string
-  Order string
+  Title string `json:"title"`
+  Order string `json:"order"`
 }
+
+type Todos struct {
+  Items []TodoItem
+}
+
+var todos Todos = Todos{}
 
 type TodoServer struct {
 }
@@ -18,16 +24,21 @@ type TodoServer struct {
 func (server *TodoServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
   if (r.Method == "POST") {
-    // var todoItem TodoItem
+    var todoItem TodoItem
     body, _ := ioutil.ReadAll(r.Body)
-    // // fmt.Println(string(body))
-    //
-    // json.Unmarshal(body, &todoItem)
-    // fmt.Println(todoItem.Title)
-    // // fmt.Printf("%+v", todoItem)
+
+    json.Unmarshal(body, &todoItem)
+    todos.Items = append(todos.Items, todoItem)
 
     fmt.Fprintf(w, string(body))
+
+  } else if (r.Method == "DELETE") {
+    todos.Items = make([]TodoItem, 0)
+    fmt.Fprintf(w, "[]")
+
+  } else if (r.Method == "GET") {
+    resultTodos, _ := json.Marshal(todos.Items)
+    fmt.Fprintf(w, string(resultTodos))
   }
-  // fmt.Fprintf(w, "Hello world! %s", r.URL.Path[1:])
 }
 
