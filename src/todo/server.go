1a041bd49c9391ce79f6f9e7cfbdcb8c2ed994ae
Sofia Tania <stania.ang@gmail.com>
2014-08-31 12:59:39 +0800

    Moved the bulk of request handling to separate methods

--- src/todo/server.go
+++ src/todo/server.go
@@ -15,38 +15,57 @@ type TodoServer struct {
 func (server *TodoServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
   if (r.Method == "POST") {
-    id := server.idGenerator.Generate()
-    var todoItem TodoItem = NewTodoItem(id)
-    body, _ := ioutil.ReadAll(r.Body)
-
-    json.Unmarshal(body, &todoItem)
-    server.todos.Add(todoItem)
-
-    fmt.Fprintf(w, todoItem.String())
+    server.HandlePOST(w, r)
 
   } else if (r.Method == "DELETE") {
-    server.todos.DeleteAll()
+    server.HandleDELETE(w, r)
 
   } else if (r.Method == "GET" && r.URL.Path == "/") {
 
-    fmt.Fprintf(w, server.todos.String())
+    server.HandleGETRoot(w, r)
 
   } else if (r.Method == "GET" && r.URL.Path != "/") {
-    todoId := r.URL.Path[1:]
-    todoItem := server.todos.Get(todoId)
-
-    fmt.Fprintf(w, todoItem.String())
+    server.HandleGETSingle(w, r)
 
   } else if (r.Method == "PATCH") {
-    todoId := r.URL.Path[1:]
-    todoItem := server.todos.Get(todoId)
-
-    body, _ := ioutil.ReadAll(r.Body)
-    json.Unmarshal(body, &todoItem)
-
-    server.todos.Update(todoItem)
-
-    fmt.Fprintf(w, todoItem.String())
+    server.HandlePATCH(w, r)
   }
 }
 
+func (server *TodoServer) HandlePOST(w http.ResponseWriter, r *http.Request) {
+  id := server.idGenerator.Generate()
+  var todoItem TodoItem = NewTodoItem(id)
+  body, _ := ioutil.ReadAll(r.Body)
+
+  json.Unmarshal(body, &todoItem)
+  server.todos.Add(todoItem)
+
+  fmt.Fprintf(w, todoItem.String())
+}
+
+func (server *TodoServer) HandlePATCH(w http.ResponseWriter, r *http.Request) {
+  todoId := r.URL.Path[1:]
+  todoItem := server.todos.Get(todoId)
+
+  body, _ := ioutil.ReadAll(r.Body)
+  json.Unmarshal(body, &todoItem)
+
+  server.todos.Update(todoItem)
+
+  fmt.Fprintf(w, todoItem.String())
+}
+
+func (server *TodoServer) HandleDELETE(w http.ResponseWriter, r *http.Request) {
+  server.todos.DeleteAll()
+}
+
+func (server *TodoServer) HandleGETRoot(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, server.todos.String())
+}
+
+func (server *TodoServer) HandleGETSingle(w http.ResponseWriter, r *http.Request) {
+  todoId := r.URL.Path[1:]
+  todoItem := server.todos.Get(todoId)
+
+  fmt.Fprintf(w, todoItem.String())
+}
